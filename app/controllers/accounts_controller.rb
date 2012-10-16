e9994b80ab4444e6741742cc77924195c1d68269
Tristan Hardy <tristan@tristanhardy.com>
2012-10-15 23:26:31 -0400

    Add account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,26 +1,38 @@
 class AccountsController < ApplicationController
   def create
     @user=current_user
-    @shared_users=@user.accounts.where(:name=>params[:account][:name]).first.users
-    @sumTransactions=0
-    @shared_users.each do |user|
-      @sumTransactions += user.transactions.where("transaction_date >= ?", Date.today).sum(:value).to_i
-    end
-    params[:account][:balance]=params[:account][:balance].to_i+@sumTransactions
     @account = Account.create(params[:account])
-    @shared_users.each do |user|
-      user.accounts<<@account
-    end  
+    @pastAccount = @user.accounts.where(:name=>params[:account][:name]).first
+    if @pastAccount
+      if @account.name=="Cash" 
+        @shared_users=@pastAccount.users.includes(:transactions).where("transaction_date >=?", Date.today)
+        @sumTransactions=0
+        @shared_users.each do |user|
+          @sumTransactions += user.transactions.where("transaction_date >= ?", Date.today).sum(:value).to_i
+        end
+        @account.balance+=@sumTransactions
+      else
+        @shared_users=@pastAccount.users
+      end
+      @shared_users.each do |user|
+        user.accounts<<@account
+      end  
+    else
+      @user.accounts<<@account
+    end
     respond_to do |format|
       format.html { redirect_to root_url}
       format.js
     end
   end
+  def index
+    @accounts=current_user.accounts.paginate(page: params[:page])
+  end
   def destroy
     @account = Account.find(params[:id])
     @account.destroy
     respond_to do |format|
-      format.html { redirect_to root_url}
+      format.html { redirect_to accounts_path}
       format.js
     end
   end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,36 +5,38 @@ class StaticPagesController < ApplicationController
       @micropost = @user.microposts.build
       #@sum_transactions = @transactions.sum(:value).to_f
       @accounts=@user.accounts
-      #@debt_acct = @accounts.where(:name=>'Targeted Debt').first
-      #@debtInitialBalance = @accounts.where(:name=>'Targeted Debt').last.balance
-      @check_acct = @accounts.where(:name=>'Cash').first
-      @shared_users = Account.where(:id=>@check_acct.id).first.users
-      @check_date = @check_acct.created_at.to_date
-      @flows = Flow.includes(:transactions)
-      @transaction = Transaction.new
-      @transactions = @user.transactions
-      if params[:date]
-        @day=Date.parse(params[:date])
-      elsif @transactions.any?
-        @day=@transactions.first.transaction_date+1
-        if @day == Date.current+1
-          @day-=1 
+      if @user.accounts.where(:name=>"Cash")
+        #@debt_acct = @accounts.where(:name=>'Targeted Debt').first
+        #@debtInitialBalance = @accounts.where(:name=>'Targeted Debt').last.balance
+        @check_acct = @accounts.where(:name=>'Cash').first
+        @shared_users = Account.where(:id=>@check_acct.id).first.users
+        @check_date = @check_acct.created_at.to_date
+        @flows = Flow.includes(:transactions)
+        @transaction = Transaction.new
+        @transactions = @user.transactions
+        if params[:date]
+          @day=Date.parse(params[:date])
+        elsif @transactions.any?
+          @day=@transactions.first.transaction_date+1
+          if @day == Date.current+1
+            @day-=1 
+          end
+        else
+          @day=Date.current
         end
-      else
-        @day=Date.current
-      end
-      @flow_sum=Hash.new
-      @flows.each do |flow|
-        @flow_sum[flow.id]=0
-        @shared_users.each do |user|
-          @flow_sum[flow.id] += user.transactions.where("transaction_date >= ? AND transaction_date <= ? AND flow_id=?", @check_date, Date.current, flow.id).sum(:value)
+        @flow_sum=Hash.new
+        @flows.each do |flow|
+          @flow_sum[flow.id]=0
+          @shared_users.each do |user|
+            @flow_sum[flow.id] += user.transactions.where("transaction_date >= ? AND transaction_date <= ? AND flow_id=?", @check_date, Date.current, flow.id).sum(:value)
+          end
         end
+        @check_acct.balance += @flow_sum[54]
+        @flow_sum.delete(54)
+        @spending_sum = @flow_sum.values.inject{|sum, x| sum+x}
+        @cash = (@check_acct.balance-@spending_sum).round
+        #@feed_items = current_user.feed.paginate(page: params[:page])
       end
-      @check_acct.balance += @flow_sum[54]
-      @flow_sum.delete(54)
-      @spending_sum = @flow_sum.values.inject{|sum, x| sum+x}
-      @cash = (@check_acct.balance-@spending_sum).round
-      #@feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
    
